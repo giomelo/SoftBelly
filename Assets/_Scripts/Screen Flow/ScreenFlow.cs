c629e78f218fac3eb833717a0a73ceaa379a61f7
Luan Assis <luanassissilva@gmail.com>
2022-03-07 21:42:25 -0300

    ScreenFlowP4
    07/03

--- Assets/_Scripts/Screen Flow/ScreenFlow.cs
+++ Assets/_Scripts/Screen Flow/ScreenFlow.cs
@@ -7,9 +7,18 @@ using UnityEngine.UI;
 
 public class ScreenFlow : MonoBehaviour
 {
-    public GameObject loadingScreen;
-    public Slider slider;
-    public Text progressText;
+    public GameObject LoadingScreen;
+    public Slider Slider;
+    public Text ProgressText;
+    public GameObject OptionsScreen;
+
+    #region Camera
+
+        public Transform Camera;
+        public Transform OptionsTarget;
+
+    #endregion
+   
 
     private void Start()
     {
@@ -21,21 +30,37 @@ public class ScreenFlow : MonoBehaviour
         StartCoroutine(LoadAsynchronously(sceneIndex));
     }
 
-   IEnumerator LoadAsynchronously(string sceneIndex)
+    IEnumerator LoadAsynchronously(string sceneIndex)
     {
         AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
 
-        loadingScreen.SetActive(true);
+        LoadingScreen.SetActive(true);
 
         while (!operation.isDone)
         {
             float progress = Mathf.Clamp01(operation.progress / .9f);
-            slider.value = progress;
-            progressText.text = progress * 100f + "%";
+            Slider.value = progress;
+            ProgressText.text = progress * 100f + "%";
             yield return null;
         }
     }
-   
+
+    #region Options Screen
+
+        public void OptionsScreenOn()
+        {
+            OptionsScreen.SetActive(true);
+            //Camera.LookAt(OptionsTarget);
+            
+        }
+
+        public void OptionsScreenOff()
+        {
+            OptionsScreen.SetActive(false);
+        }
+
+    #endregion
+    
 
     public void BtnQuit()
     {
