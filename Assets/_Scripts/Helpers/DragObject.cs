54239c54f17a19fa7a9f38539c1df2012f48a28d
Giovana Melo <giovanamelomr@gmail.com>
2022-04-25 10:24:02 -0300

    Better drag

---
+++ Assets/_Scripts/Helpers/DragObject.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using _Scripts.Singleton;
+using _Scripts.Systems.Lab;
+using _Scripts.Systems.Lab.Machines;
+using UnityEngine;
+
+namespace _Scripts.Helpers
+{
+    public class DragObject : MonoBehaviour
+    {
+        private Vector3 mOffset;
+        private float mZCoord;
+
+        private void OnMouseDown()
+        {
+            var position = gameObject.transform.position;
+            mZCoord = GameManager.Instance.MainCamera.WorldToScreenPoint(
+                position).z;
+            // Store offset = gameobject world pos - mouse world pos
+            mOffset = position - GetMouseAsWorldPoint();
+            Cursor.visible = false;
+        }
+
+        private void OnMouseUp()
+        {
+            Cursor.visible = true;
+        }
+
+
+        private Vector3 GetMouseAsWorldPoint()
+        {
+            // Pixel coordinates of mouse (x,y)
+            Vector3 mousePoint = Input.mousePosition;
+
+            // z coordinate of game object on screen
+            mousePoint.z = mZCoord;
+            // Convert it to world points
+
+            return GameManager.Instance.MainCamera.ScreenToWorldPoint(mousePoint);
+        }
+        
+        private void OnMouseDrag()
+        {
+            if (LabEvents.CurrentMachine == null ) return;
+            if (LabEvents.CurrentMachine as Pestle)
+            {
+                transform.position = GetMouseAsWorldPoint() + mOffset;
+            }
+        }
+    }
+}
--- Assets/_Scripts/Systems/Lab/Machines/Pestle.cs
+++ Assets/_Scripts/Systems/Lab/Machines/Pestle.cs
@@ -16,37 +16,37 @@ namespace _Scripts.Systems.Lab.Machines
 
         private void Update()
         {
-            if (LabEvents.CurrentMachine == null || LabEvents.CurrentMachine.MachineId != MachineId) return;
-            if (Input.GetMouseButton(0))
-            {
-                if (DragAndDropBase.SelectedObject == null)
-                {
-                    RaycastHit hit = DragAndDropBase.CastRay();
-                    if (hit.collider == null) return;
-                    if (!hit.collider.CompareTag("Drag")) return;
-                
-                    DragAndDropBase.SelectedObject = hit.collider.gameObject;
-                    Cursor.visible = false;
-                }
-                else
-                {
-                    //disable rigidybody
-                    // DragAndDropBase.SelectedObject =null;
-                    // Cursor.visible = true;
-                }
-
-                if (DragAndDropBase.SelectedObject == null) return;
-                Vector3 pos = new Vector3(Input.mousePosition.x, Input.mousePosition.y,
-                    GameManager.Instance.MainCamera
-                        .WorldToScreenPoint(DragAndDropBase.SelectedObject.transform.position).z);
-                Vector3 worldPos = GameManager.Instance.MainCamera.ScreenToWorldPoint(pos);
-                DragAndDropBase.SelectedObject.transform.position = new Vector3(worldPos.x, worldPos.y, worldPos.z);
-            }
-            else
-            {
-                DragAndDropBase.SelectedObject =null;
-                Cursor.visible = true;
-            }
+            // if (LabEvents.CurrentMachine == null || LabEvents.CurrentMachine.MachineId != MachineId) return;
+            // if (Input.GetMouseButton(0))
+            // {
+            //     if (DragAndDropBase.SelectedObject == null)
+            //     {
+            //         RaycastHit hit = DragAndDropBase.CastRay();
+            //         if (hit.collider == null) return;
+            //         if (!hit.collider.CompareTag("Drag")) return;
+            //     
+            //         DragAndDropBase.SelectedObject = hit.collider.gameObject;
+            //         Cursor.visible = false;
+            //     }
+            //     else
+            //     {
+            //         //disable rigidybody
+            //         // DragAndDropBase.SelectedObject =null;
+            //         // Cursor.visible = true;
+            //     }
+            //
+            //     if (DragAndDropBase.SelectedObject == null) return;
+            //     Vector3 pos = new Vector3(Input.mousePosition.x, Input.mousePosition.y,
+            //         GameManager.Instance.MainCamera
+            //             .WorldToScreenPoint(DragAndDropBase.SelectedObject.transform.position).z);
+            //     Vector3 worldPos = GameManager.Instance.MainCamera.ScreenToWorldPoint(pos);
+            //     DragAndDropBase.SelectedObject.transform.position = new Vector3(worldPos.x, worldPos.y, worldPos.z);
+            // }
+            // else
+            // {
+            //     DragAndDropBase.SelectedObject =null;
+            //     Cursor.visible = true;
+            // }
 
         }
     }
