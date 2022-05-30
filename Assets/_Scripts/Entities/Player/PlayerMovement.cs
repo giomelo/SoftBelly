93414815eb1a70536b6a58415bd85fe05ee4b8f4
Luan Assis <luanassissilva@gmail.com>
2022-05-30 11:26:10 -0300

    MovFix
    Mudei a orientação do cenario no gardem para que a movimentação funcionasse, arrumei a rotação do personagem tambem

--- Assets/_Scripts/Entities/Player/PlayerMovement.cs
+++ Assets/_Scripts/Entities/Player/PlayerMovement.cs
@@ -19,7 +19,7 @@ namespace _Scripts.Entities.Player
         {
             TryGetComponent(out _chController);
         }
-    
+
         /// <summary>
         /// Move character controller based on axis "horizontal" and "vertical"
         /// </summary>
@@ -27,23 +27,17 @@ namespace _Scripts.Entities.Player
         /// <param name="vertical"></param>
         public void ProcessInput(float horizontal, float vertical)
         {
-            // var move = new Vector3(horizontal, 0, vertical);
-            // move.y += gravityValue * Time.deltaTime;
-            // _chController.Move(move * Time.deltaTime * _playerSpeed);
-            // //gameObject.transform.forward = move;
-        }
-        
-        void Move()
-        {
-            
-            _chController.Move( Time.deltaTime * _playerSpeed * Input.GetAxis("Vertical") * transform.forward);
+            var move = new Vector3(horizontal, 0, vertical);
+            //move.y += gravityValue * Time.deltaTime;
+            _chController.Move(move * Time.deltaTime * _playerSpeed);
 
-            transform.Rotate(0,Input.GetAxis("Horizontal") * 180 * Time.deltaTime,0);
-  
-        }
-        private void Update()
-        {
-            Move();
+            if (move != Vector3.zero)
+            {
+                //gameObject.transform.forward = move;
+                Quaternion toRotation = Quaternion.LookRotation(move, Vector3.up);
+                transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, 720 * Time.deltaTime);
+            }
+
         }
     }
 }
