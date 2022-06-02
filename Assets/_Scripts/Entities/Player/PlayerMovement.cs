450fd97a8b8f560585208e44943dba26781afa25
Giovana <giovanamelomr@gmail.com>
2022-06-02 13:40:14 -0300

    Gravidade

--- Assets/_Scripts/Entities/Player/PlayerMovement.cs
+++ Assets/_Scripts/Entities/Player/PlayerMovement.cs
@@ -11,6 +11,8 @@ namespace _Scripts.Entities.Player
 
         private float _playerSpeed = 10f;
         private Vector3 _playerVelocity;
+        [SerializeField]
+        private float gravity = 2f;
 
         private const float gravityValue = -12;
 
@@ -27,14 +29,14 @@ namespace _Scripts.Entities.Player
         /// <param name="vertical"></param>
         public void ProcessInput(float horizontal, float vertical)
         {
-            var move = new Vector3(horizontal, 0, vertical);
+            var move = new Vector3(horizontal, gravity, vertical);
             //move.y += gravityValue * Time.deltaTime;
             _chController.Move(move * Time.deltaTime * _playerSpeed);
-
+            Vector3 angle = new Vector3(move.x, 0, move.z);
             if (move != Vector3.zero)
             {
                 //gameObject.transform.forward = move;
-                Quaternion toRotation = Quaternion.LookRotation(move, Vector3.up);
+                Quaternion toRotation = Quaternion.LookRotation(angle, Vector3.up);
                 transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, 720 * Time.deltaTime);
             }
 
