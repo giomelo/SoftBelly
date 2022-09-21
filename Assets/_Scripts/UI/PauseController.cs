60b26c8d6b0cce12c8f53c63103aa85f189ca3e6
VitorPassos <vitorpassos48@hotmail.com>
2022-09-20 22:58:33 -0300

    Primeira versão de menu de pausa

--- Assets/_Scripts/UI/PauseController.cs
+++ Assets/_Scripts/UI/PauseController.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using _Scripts.Screen_Flow;
 using UnityEngine;
 
@@ -23,40 +24,57 @@ namespace _Scripts.UI
         [SerializeField]
         private GameObject optionsObj;
         private GameObject currentMenu;
+
+        //Mudanças de nova UI
+        [SerializeField]
+        private int menuState = 0;
+        [SerializeField]
+        private int satelliteMenuState = 0;
+        [SerializeField]
+        private Animator anim;
         
         private void Update()
         {
-            if (!Input.GetKeyDown(KeyCode.Escape)) return;
-            PauseInput();
+            if (Input.GetKeyDown(KeyCode.Escape) && menuState != -1) { PauseInput(); }
         }
 
         private void PauseInput()
         {
-            pauseObj.SetActive(!pauseObj.activeInHierarchy);
-            Time.timeScale = pauseObj.activeInHierarchy ? 0 : 1;
+            StartCoroutine("SHMenu");
         }
-        
+
         public void PauseButton(ButtonsComponents button)
         {
             
             switch (button.Button)
             {
-                
-                case UI.PauseButton.Controlls:
-                    Debug.Log("Contorle");
-                    controllsObj.SetActive(true);
-                    currentMenu = controllsObj;
-                    break;
                 case UI.PauseButton.Resume:
-                    pauseObj.SetActive(false);
-                    Time.timeScale = 1;
+                    PauseInput();
                     break;
-                case UI.PauseButton.Options:
-                    // optionsObj.SetActive(true);
-                    // currentMenu = optionsObj;
-                    break;
-                case UI.PauseButton.Back:
-                    currentMenu.SetActive(false);
+                case UI.PauseButton.Options:    //Ambos executam o mesmo código com poucas alterações
+                case UI.PauseButton.Controlls:
+                    if (menuState == 1)
+                    {
+                        string[] anims = new string[3];
+                        if (button.Button == UI.PauseButton.Options) { anims = new string[3] { "PauseAnim.ShowOptions", "PauseAnim.HideOptions", "PauseAnim.HideContShowOp" }; }
+                        if (button.Button == UI.PauseButton.Controlls) { anims = new string[3] { "PauseAnim.ShowControl", "PauseAnim.HideControl", "PauseAnim.HideOpShowCont" }; }
+                        int am = button.Button == UI.PauseButton.Options ? 2 : 1;
+                        int other = button.Button == UI.PauseButton.Options ? 1 : 2;
+                        menuState = 2;
+                        PauseInput();
+                        if (satelliteMenuState == 0) {
+                            anim.Play(anims[0]);
+                            satelliteMenuState = am;
+                        }
+                        else if (satelliteMenuState == am) { 
+                            anim.Play(anims[1]);
+                            satelliteMenuState = 0;
+                        }
+                        else if (satelliteMenuState == other) { 
+                            anim.Play(anims[2]);
+                            satelliteMenuState = am;
+                        }
+                    }
                     break;
                 case UI.PauseButton.MainMenu:
                     pauseObj.SetActive(false);
@@ -66,6 +84,46 @@ namespace _Scripts.UI
                 case UI.PauseButton.Exit:
                     Time.timeScale = 1;
                     Application.Quit();
+                    break;
+            }
+        }
+
+        private IEnumerator SHMenu()
+        {
+            float startTime = Time.realtimeSinceStartup;    //realtimeSinceStartup é usado aqui pois os yield waitfors das corotinas não funcionam com timescale 0
+            switch (menuState)
+            {
+                case -1:
+                    break;
+
+                case 0:
+                    menuState = -1;
+                    pauseObj.SetActive(!pauseObj.activeInHierarchy);
+                    Time.timeScale = 0;
+                    anim.Play("PauseAnim.ShowPause");
+                    while (Time.realtimeSinceStartup - startTime < 1.1f) { yield return null; }
+                    menuState = 1;
+                    break;
+
+                case 1:
+                    menuState = -1;
+                    if (satelliteMenuState == 1)
+                        anim.Play("PauseAnim.HidePauseSatCont");
+                    else if (satelliteMenuState == 2)
+                        anim.Play("PauseAnim.HidePauseSatOpt");
+                    else
+                        anim.Play("PauseAnim.HidePause");
+                    while (Time.realtimeSinceStartup - startTime < 1.1f) { yield return null; }
+                    satelliteMenuState = 0;
+                    menuState = 0;
+                    pauseObj.SetActive(!pauseObj.activeInHierarchy);
+                    Time.timeScale = 1;
+                    break;
+
+                case 2:
+                    menuState = -1;
+                    while (Time.realtimeSinceStartup - startTime < 1.1f) { yield return null; }
+                    menuState = 1;
                     break;
             }
         }
