45d71fe57effb7df6794dfae287926f73dca19b7
Giovana Melo <giovanamelomr@gmail.com>
2022-09-28 10:17:06 -0300

    Scroll da loja e balanceamento

---
+++ Assets/_Scripts/Helpers/BalanceControl.cs
@@ -0,0 +1,14 @@
+﻿namespace _Scripts.Helpers
+{
+    public class BalanceControl
+    {
+        public static float EasyPotion = 95;
+        public static float MediumPotion = 5;
+        public static float HardPotion = 0;
+
+        private void ChangeDay()
+        {
+            
+        }
+    }
+}
