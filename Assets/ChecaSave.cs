2c2634a5245833afd70e0f995875629105276b38
VitorPassos <vitorpassos48@hotmail.com>
2022-11-07 14:11:47 -0300

    Opção LOAD no menu principal

---
+++ Assets/ChecaSave.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ChecaSave : MonoBehaviour
+{
+    [SerializeField]
+    private FadeIO fade;
+    [SerializeField]
+    private MenuSceneManager scene;
+
+    private bool SaveExiste()
+    {
+        return false;
+    }
+
+    public void ConfirmaNovo()
+    {
+        if (SaveExiste())
+        {
+
+        } else
+        {
+            fade.StartFade();
+            scene.ChangeWithDelay("SC_Garden");
+        }
+    }
+
+    private void Start()
+    {
+        if (gameObject.name == "Carregar" && !SaveExiste()) 
+        {
+            Color transp = Color.black;
+            transp.a = 0.92f;
+            gameObject.GetComponent<Button>().interactable = false;
+            gameObject.GetComponentInChildren<Text>().color = transp;
+            gameObject.GetComponent<EventTrigger>().enabled = false;
+        }
+    }
+}
